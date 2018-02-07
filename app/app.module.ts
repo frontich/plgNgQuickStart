e586e17d0571473b1ed48f812b6f2dc73eca4f42
frontich <bostjan267@gmail.com>
2018-02-07 19:53:55 +0100

    add comments for basic steps

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 
 // This decorator describes the class that follows it
+// @NgModule is a function, it is also decorator
 @NgModule({
   imports:      [ BrowserModule ], // What stuff do I need?
   declarations: [ AppComponent ],  // What's in my app module?
   bootstrap:    [ AppComponent ]   // Where do I start?
 })
+/** metadata is attached to AppModule */
 export class AppModule { }
--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,10 @@
+/** javascript import statement */
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+/** AppModule == simbol, you can find it inside app.module  */
 import { AppModule } from './app.module';
 
-// Start here --> AppModule
+/** dynamic platform browser vrs static browser platform :) 
+ *  boostrap module
+*/
 platformBrowserDynamic().bootstrapModule(AppModule);

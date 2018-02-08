0eff7c14504f9e5482848b628055ffbaa74c9ea9
frontich <bostjan267@gmail.com>
2018-02-08 18:30:32 +0100

    selector.value vs .target.value

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,13 +7,37 @@ import { Component } from '@angular/core';
   <fieldset>
     <img [src]="image">
   </fieldset>
+  <label [style.color]="color">Favorite color: </label>
+  <button (click)="clicked()">Toggle color</button>
+  <select (change)="colorChange($event.target.value)">
+    <option>red</option>
+    <option>grey</option>
+    <option>orange</option>
+    
+  </select>
   `
 })
-export class AppComponent  {
-  name = 'Alex Smith';
-  image ='favicon.ico'
+export class AppComponent {
+  name = 'Janez Novak';
+  image = 'favicon.ico';
+  color = "green";
+
+  clicked() {
+    this.color = this.color === "green" ? "blue" : "green";
+  }
+
+  colorChange(color: string) {
+    this.color = color;
+  }
 }
 
-/** interpolation      == <img src={{image}}
+/** 
+ * interpolation      == <img src={{image}}
  *  property binding   == <img [src]="image"
+ * 
+ */
+
+/**
+ * #selector && selector.value 
+ * $event.target.value
  */

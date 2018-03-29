4441ff2dbc6a5d701d955a2c0f663e09334cc0b0
Boštjan Koželj <bostjan.kozelj@ibx.si>
2018-03-29 19:12:46 +0200

    implement customers list and add some styling

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,60 +4,40 @@ import { Customer } from './model';
 
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app', // to use me, put <my-app> in index.html
-  template: `
-  <h1>{{ customer.name }}</h1>
-  <p><i>{{ customer.name }} is at {{ customer.address.street }} in {{ customer.address.city }} in the {{ customer.address.region }} region.</i></p>
-  <br/>
-  <fieldset>
-    <input [(ngModel)]="customer.name">
-    <br/>
-  </fieldset>  
-
-  <label><input type="checkbox" [(ngModel)]="hideAddress">Hide address</label>
-
-  <div [hidden]="hideAddress">
-  <fieldset>
-    <label>Street: <input [(ngModel)]="customer.address.street"> </label>
-  </fieldset>
-  <fieldset>
-    <label>City: <input [(ngModel)]="customer.address.city"> </label>
-  </fieldset>
-    <fieldset>
-      <label>State: </label> 
-       <select [(ngModel)]="customer.address.state">
-        <option>Gorenjska</option>
-        <option>Osrednjeslovenska</option>
-        <option>Primorska</option>
-        <option>Prekmurska</option>
-      </select>
-    </fieldset>
-    <fieldset>
-    <label>Region: </label> 
-     <select [(ngModel)]="customer.address.region">
-      <option>East</option>
-      <option>North</option>
-      <option>South</option>
-      <option>West</option>
-    </select>
-  </fieldset>
-  </div>
-  `
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
 
-  hideAddress = false;
-  customer: Customer = {
-    id: 1,
-    name: 'Janez Novak',
-    address: {
-      street: 'Unknown street 666',
-      city: 'Lost City',
-      region: 'East',
-      state: 'Gorenjska'
-    }
-  }
+  regions = ['vzhod', 'zahod', 'sever', 'jug', 'severozahod'];
 
+  customers: Customer[] = [
+    {
+      id: 1,
+      name: 'Janez Novak',
+      address: {
+        street: 'Unknown street 666',
+        city: 'Lost City',
+        region: 'severozahod',
+        state: 'Gorenjska'
+      }
+    },
+    {
+      id: 2,
+      name: 'Marija Novak',
+      address: {
+        street: 'Unknown street 001',
+        city: 'Found City',
+        region: 'vzhod',
+        state: 'Primorska'
+      }
+    }
+  ];
+
+  customer: Customer = this.customers[1];
+  hideAddress = false;
 }
 
 /** 

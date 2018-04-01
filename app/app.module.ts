11622a01e1317978fe60bf1d7d7534f5965b4132
frontich <bostjan267@gmail.com>
2018-04-01 21:20:38 +0200

    fix function name typo, import dev http service

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,24 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
 import { CustomerListComponent } from './customer-list.component';
 import { CustomerDetailComponent } from './customer-detail.component';
 import { AddressComponent } from './address.component';
 import { DataService } from './data.service';
-import { LoggerService} from './logger.service';
+import { LoggerService } from './logger.service';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 // This decorator describes the class that follows it
 // @NgModule is a function, it is also decorator
 @NgModule({
-  imports: [BrowserModule, FormsModule], // What stuff do I need?
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)], // What stuff do I need?
   declarations: [AppComponent,
     CustomerListComponent,
     CustomerDetailComponent,
     AddressComponent
   ],
-  providers : [DataService, LoggerService], // What's in my app module?
+  providers: [DataService, LoggerService], // What's in my app module?
   bootstrap: [AppComponent]   // Where do I start?
 })
 /** metadata is attached to AppModule */
--- app/data.service.ts
+++ app/data.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
 
 import { createTestCustomers } from './test-data';
 import { LoggerService } from './logger.service';
@@ -7,25 +8,43 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/toPromise';
 
 
 @Injectable()
 export class DataService {
+    private customersUrl = 'api/customers';
 
-    constructor(private loggerService: LoggerService) { }
+    constructor(
+        private loggerService: LoggerService,
+        private http: Http
+    ) { }
 
     getCustomersAsPromise(): Promise<Customer[]> {
 
-        this.loggerService.log('Getting customers as a Promise ...');
-        const customers = createTestCustomers();
+        this.loggerService.log('Getting customers as a Promise via HTTP...');
+
+        return this.http.get(this.customersUrl)
+            .toPromise()
+            .then(response => {
+                const cust = response.json().data as Customer[];
+                this.loggerService.log(`Got ${cust.length} customers`);
+                return cust;
+            },
+                error => {
+                    this.loggerService.log(`An error occured ${error}`);
+                    return Promise.reject('Something bad happend, check the console logs...');
+                }
+            );
+        // const customers = createTestCustomers();
         // fake a promise
-        return new Promise<Customer[]>(resolve => {
+        /*return new Promise<Customer[]>(resolve => {
             setTimeout(() => {
                 this.loggerService.log(`Got ${customers.length} customers`);
                 resolve(customers);
 
             }, 1500);
-        });
+        });*/
     }
 
     getCustomersAsObservable(): Observable<Customer[]> {

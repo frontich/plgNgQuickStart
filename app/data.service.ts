f79661d30958a32731bc3bf8ab133ba43f56a309
frontich <bostjan267@gmail.com>
2018-04-01 19:56:13 +0200

    add string interpolation for logger log method

--- app/data.service.ts
+++ app/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { createTestCustomers  } from './test-data';
+import { createTestCustomers } from './test-data';
 import { LoggerService } from './logger.service';
 
 @Injectable()
@@ -8,9 +8,10 @@ export class DataService {
 
     constructor(private loggerService: LoggerService) { }
 
-    getCustomers(){
-        this.loggerService.log('I have got ' + createTestCustomers().length + ' customers');
-        return createTestCustomers();
+    getCustomers() {
+        const customers = createTestCustomers();
+        this.loggerService.log(`Got ${customers.length} customers`);
+        return customers;
 
     }
 }
